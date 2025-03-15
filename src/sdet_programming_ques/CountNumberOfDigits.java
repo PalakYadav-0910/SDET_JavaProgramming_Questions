21fdebca53c4e6fd9c9e821abd4975c8517eb142
Palak_Yadav <palak.yadav-EXT@lebara.com>
2025-03-15 13:33:01 +0530

    Count number of digits in a Number

---
+++ src/sdet_programming_ques/CountNumberOfDigits.java
@@ -0,0 +1,19 @@
+package sdet_programming_ques;
+
+public class CountNumberOfDigits {
+
+    public static void main(String[] args) {
+
+        //Count Number of Digits in a Number
+
+        int num = 123456;
+        int count = 0;
+
+        while(num>0){
+            num = num /10;
+            count++;
+        }
+
+        System.out.println("Number of Digits are : " + count);
+    }
+}
