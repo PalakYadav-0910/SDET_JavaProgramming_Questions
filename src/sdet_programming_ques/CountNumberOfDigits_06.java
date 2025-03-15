3f9771b912d2c224d28736b1c3e9883be467c0d5
Palak_Yadav <palak.yadav-EXT@lebara.com>
2025-03-15 14:28:02 +0530

    Count number of digits in a number

--- src/sdet_programming_ques/CountNumberOfDigits_06.java
+++ src/sdet_programming_ques/CountNumberOfDigits_06.java
@@ -5,7 +5,6 @@ public class CountNumberOfDigits_06 {
     public static void main(String[] args) {
 
         //Count Number of Digits in a Number
-
         int num = 123456;
         int count = 0;
 
